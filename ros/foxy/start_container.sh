885980cc0a6c820ef2865f41e405d99afac7db8a
Jilada Eccleston <jilada.eccleston@gmail.com>
2020-08-28 15:03:01 +0900

    Specify configuration files from dotfiles folder

--- ros/foxy/start_container.sh
+++ ros/foxy/start_container.sh
@@ -12,8 +12,13 @@ docker run -it \
     --user $UID:$GID \
     --hostname $HOSTNAME \
     --workdir=$HOME_DIR \
+    --net=host \
     -v "/etc/group:/etc/group:ro" \
     -v "/etc/passwd:/etc/passwd:ro" \
-    -v"/etc/shadow:/etc/shadow:ro" \
-    -v "/home/$USER:/home/$USER" \
+    -v "/etc/shadow:/etc/shadow:ro" \
+    -v "$HOME_DIR/github:$HOME_DIR/github" \
+    -v "$HOME_DIR/data:$HOME_DIR/data" \
+    -v "$HOME_DIR/.zsh:$HOME_DIR/.zsh" \
+    -v "$HOME_DIR/.zshenv:$HOME_DIR/.zshenv" \
+    -v "$HOME_DIR/.tmux.conf:$HOME_DIR/.tmux.conf" \
     $CONTAINER_NAME:$CONTAINER_TAG /bin/zsh
